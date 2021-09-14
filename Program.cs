f0c8a7a3fcd0e7d1d36d47b3d7d18a4df9ce266b
savagecabbage123 <mine268253@gmail.com>
2021-09-14 07:27:22 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Array_challenge
+namespace Calculator
 {
     class Program
     {
-        static void Main()
+        
+        static void Main(string[] args)
         {
-            Console.WriteLine("how many students are in your class?");
-            int numstudents;
-            numstudents = Convert.ToInt32(Console.ReadLine());
-            String[] students = new string[numstudents];
-            Console.WriteLine("Type in the names of the number of students");
-            for (int i = 0; i < students.Length; i++)
+            //a string list called opList
+            List<string> opList = new List<string>();
+            opList.Add("/");
+            opList.Add("+");
+            opList.Add("-");
+            opList.Add("*");
+            //writes calculator
+            Console.WriteLine("Calculator");
+            //writes put in a number
+            Console.Write("put in a number: ");
+            //lets you input something and is called num1
+            string num1 = Console.ReadLine();
+            double number;
+            double number2;
+            number = Convert.ToDouble(num1);
+            //writes put in another number
+            Console.Write("put in another number: ");
+            //lets you input something and is called num2
+            string num2 = Console.ReadLine();
+            number2 = Convert.ToInt32(num2);
+            //tells what you chose on both num vars and asks what calculation you want
+            Console.Write("This is your first number {0}", num1);
+            Console.WriteLine(" and this is your second number {0}", num2);
+            Console.WriteLine("Choose what operation you would like to do with these two numbers: ");
+            //displays your list you made
+            foreach (string a in opList)
+                Console.WriteLine(a);
+            //lets you input your operator
+            string op = Console.ReadLine();
+            //function called calculate when it is called it does a small "animation" with text
+            void Calculate()
             {
-                students[i] = Console.ReadLine();
+                Console.Write("Calculating.");
+                //tells program to wait between each line
+                System.Threading.Thread.Sleep(700);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(700);
+                Console.WriteLine(".");
+                System.Threading.Thread.Sleep(700);
             }
-            Console.WriteLine("here they are alphabetically");
-            Array.Sort(students);
-            for (int i = 0; i < students.Length; i++)
+            //checks if your input is equal to this operator on the list 
+            if (op == "/")
             {
-                Console.WriteLine(students[i]);
+                //calls the calculate function
+                Calculate();
+                //calculates results
+                Console.WriteLine("Results: ", number / number2);
+            }
+            if (op == "*")
+            {
+                //calls the calculate function
+                Calculate();
+                //calculates results
+                Console.WriteLine("Results: ", number * number2);
+            }
+            if (op == "+")
+            {
+                //calls the calculate function
+                Calculate();
+                //calculates results
+                Console.WriteLine("Results: ", number + number2);
+            }
+            if (op == "-")
+            {
+                //calls the calculate function
+                Calculate();
+                //calculates results
+                Console.WriteLine("Results: ", number - number2);
             }
         }
     }
