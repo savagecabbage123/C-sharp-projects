d0d761d61d3ff922eb26b0651fc77fa2dda5b4c6
savagecabbage123 <mine268253@gmail.com>
2021-09-14 07:25:23 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace word_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a sentence: ");
+            wordcount();
+        }
+        static void wordcount()
+        {
+            string sentence = Console.ReadLine();
+            int count = sentence.Split(' ').Length;
+            Console.WriteLine("the sentence has " + count + " words");
+        }
+    }
+}
---
+++ pressureplate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressureplate : MonoBehaviour
+{
+    public Animator animator;
+    public bool weighton;
+    void Start()
+    {
+        animator.SetBool("weight_on", false);
+        weighton = false;
+    }
+    public void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player") || player.gameObject.CompareTag("rock"))
+        {
+            animator.SetBool("weight_on", true);
+            weighton = true;
+        }
+        if (player.gameObject.CompareTag("Player") && player.gameObject.CompareTag("rock"))
+        {
+            animator.SetBool("weight_on", true);
+            weighton = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player") && player.gameObject.CompareTag("rock"))
+        {
+            animator.SetBool("weight_on", false);
+            weighton = false;
+        }
+        if (player.gameObject.CompareTag("Player") || player.gameObject.CompareTag("rock"))
+        {
+            animator.SetBool("weight_on", false);
+            weighton = false;
+        }
+    }
+    public void OnTriggerStay2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player") && player.gameObject.CompareTag("rock"))
+        {
+            animator.SetBool("weight_on", true);
+            weighton = true;
+        }
+        if (player.gameObject.CompareTag("Player") || player.gameObject.CompareTag("rock"))
+        {
+            animator.SetBool("weight_on", true);
+            weighton = true;
+        }
+    }
+}
