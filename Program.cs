1123b79fb2e6e47860967bb8a6b0b385a783bce6
savagecabbage123 <mine268253@gmail.com>
2021-09-14 07:26:10 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 
-namespace word_counter
+namespace Array_challenge
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("enter a sentence: ");
-            wordcount();
-        }
-        static void wordcount()
-        {
-            string sentence = Console.ReadLine();
-            int count = sentence.Split(' ').Length;
-            Console.WriteLine("the sentence has " + count + " words");
+            Console.WriteLine("how many students are in your class?");
+            int numstudents;
+            numstudents = Convert.ToInt32(Console.ReadLine());
+            String[] students = new string[numstudents];
+            Console.WriteLine("Type in the names of the number of students");
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = Console.ReadLine();
+            }
+            Console.WriteLine("here they are alphabetically");
+            Array.Sort(students);
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
         }
     }
 }
